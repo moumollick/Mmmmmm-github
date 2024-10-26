431a3c572e9fa37e59e361fb33d86e0551852985
Mou Mollick <141056468+moumollick@users.noreply.github.com>
2024-10-26 18:21:44 +0600

    Vowel& consonant

---
+++ Mou.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    char vowel []= {'a','e','i','o','u','A','E','I','O','U'};
+    for(int i=0; i<10; i++) {
+        if(ch=vowel[i]) {
+            printf("%c is a vowel\n",ch);
+            return 0;
+        }
+    }
+
+    if ((ch>'a'&&ch<='z'||(ch>'A'&&ch<='Z')){
+            printf("%c is a consonent \n",ch);
+            else
+                printf("%c is not an Alphabet \n",ch);
+                return 0;
+    }
